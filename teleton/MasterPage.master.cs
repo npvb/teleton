012b767bce4cb9e96324e02adac5b9d10362ec2d
Sergio Peralta <serpel_js@hotmail.com>
2011-06-23 02:43:51 -0600

    terminado logout

--- teleton/MasterPage.master.cs
+++ teleton/MasterPage.master.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Security;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -10,5 +11,10 @@ public partial class MasterPage : System.Web.UI.MasterPage
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void labelLogout_Click(object sender, EventArgs e)
+    {
+        FormsAuthentication.SignOut();
+        FormsAuthentication.RedirectToLoginPage();
     }
 }
